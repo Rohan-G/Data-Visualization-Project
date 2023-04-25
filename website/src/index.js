95c6cda2c0e919c81ce46da2c3fc2c431249ef2e
Rohan <Rohan.Girish.Aradhya@gmail.com>
2023-04-26 00:44:31 +0530

    Added Regulations Information

--- website/src/index.js
+++ website/src/index.js
@@ -8,8 +8,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    {/* <Dominance /> */}
+    {/* <App /> */}
+    <Dominance />
   </React.StrictMode>
 );
 
